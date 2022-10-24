bed74c88c63347db470151b68358fe90ff779f0f
sapnaitgk <95950405+sapnaitgk@users.noreply.github.com>
2022-10-24 17:02:06 +0530

    Update a.js
    Signed-off-by: sapnaitgk <95950405+sapnaitgk@users.noreply.github.com>

--- a.js
+++ a.js
@@ -1,1 +1,6 @@
-
+   var downloadUrl = document.querySelectorAll(".safe");
+   for (let i = 0; i < downloadUrl.length; i++) {
+            const dlink = downloadUrl[i].href;
+     		var uncodeurl = dlink.href = btoa(dlink);
+     		downloadUrl[i].href = safepageurl + "?o=" + uncodeurl;
+        };
